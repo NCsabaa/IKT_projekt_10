d48faac04687e9110892c12c390bba33344b1aa5
Nagy Csaba <90310997+NCsabaa@users.noreply.github.com>
2021-09-22 09:40:54 +0200

    Add files via upload

---
+++ .py
@@ -0,0 +1,7 @@
+def bundáskenyér(cola_type):
+    print ('A tojásokat egy mélytányérba ütjük,és villávalfelverjük.')
+    print ('A kenyérszeleteketvagy félbe vagy egészben belerakjuk és megforgatjuk a tojásban.')
+    print ('Hagyjuk hogy egy kicsit megszívja magát tojással')
+    print ('Egy serpenyőbe kevés olajat teszünl majd ha felforrósódott akkor bele tesszük a kenyérszeleteket.')
+    print('Szolgáld fel'+ cola_type + 'val/vel.')
+    print ('coca cola')
