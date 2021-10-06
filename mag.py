7794b915447216c37cfde05ea28a39fae147948b
Nagy Csaba <90310997+NCsabaa@users.noreply.github.com>
2021-10-06 10:37:16 +0200

    Add files via upload

---
+++ mag.py
@@ -0,0 +1,11 @@
+
+magasság =int (input ('Mennyi a magasságod?: '))
+
+if (magasság < 180):
+    print("Törpe vagy")
+    
+elif (magasság == 180):
+    print ('Normális a magasságod')
+
+else:
+    print ("Zsiráf vagy")
