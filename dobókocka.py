bfff5ebaa900a785d7b85f7d110beb38a95dc5af
Nagy Csaba <90310997+NCsabaa@users.noreply.github.com>
2021-10-20 13:43:17 +0200

    Add files via upload

---
+++ dobókocka.py
@@ -0,0 +1,27 @@
+import random
+x=random.randint(1,6)
+print( x )
+if x==1:
+        print("[ ]     [ ]")
+        print("[ ] [1] [ ]")
+        print("[ ]     [ ]")
+elif x==4:
+        print("[4]     [4]")
+        print("[ ] [ ] [ ]")
+        print("[4]     [4]")
+elif x==3:
+        print("[3]     [3]")
+        print("[ ] [3] [ ]")
+        print("[ ]     [ ]")
+elif x==2:
+        print("[2]     [2]")
+        print("[ ] [ ] [ ]")
+        print("[ ]     [ ]")
+elif x==5:
+        print("[5]     [5]")
+        print("[5] [5] [5]")
+        print("[5]     [5]")
+elif x==6:
+        print("[6]  [6]")
+        print("[6]  [6]")
+        print("[6]  [6]")
